0201762f94a232b610967284d1ec7a15e72d0504
arrowanwar <arrowanwar@gmail.com>
2023-01-14 19:53:33 +0600

    mvc start

--- app/core/app.php
+++ app/core/app.php
@@ -16,19 +16,25 @@ class app {
     private $param = [];
     public function __construct() {
         $url = $this->splitURL();
-        if(file_exists("../app/contollers/".strtolower($url[0]).".php")){
-            $this->controller = strtolower($url[0]);
-            unset($url[0]);
-        }
-        require '../controllers/'.$this->controller.'.php';
-        $this->controller = new $this->controller;
-        if(isset($url[1])){
-            if(method_exists($this->controller, $url[1])){
-                $this->method = $url[1];
-                unset($url[1]);
-            }
-        }
         show($url);
+//        require '../controllers/home.php';
+        if(file_exists("../app/controllers/home.php")){
+            echo 'ok';
+//            $this->controller = strtolower($url[0]);
+//            unset($url[0]);
+//            show($url);
+        } else {
+            echo 'not';
+        }
+//        require '../controllers/home.php';
+//        $this->controller = new $this->controller;
+//        if(isset($url[1])){
+//            if(method_exists($this->controller, $url[1])){
+//                $this->method = $url[1];
+//                unset($url[1]);
+//            }
+//        }
+//        show($url);
     }
     private function splitURL() {
         return explode("/", trim($_GET['url'],"/"));
