9a1615f996d8f49f6ac3899d405818a83b3c7d02
arrowanwar <47405289+arrowanwar@users.noreply.github.com>
2023-01-24 21:01:57 +0600

    update about.php

---
+++ app/controllers/about.php
@@ -0,0 +1,13 @@
+class about {
+    function index()
+    {
+        $this->view("about");
+    }
+    function view($view){
+         if(file_exists("../app/views/".$view.".php")){
+             include "../app/views/".$view.".php";
+        } else {
+            include "../app/views/404.php";
+        }
+    }
+}
